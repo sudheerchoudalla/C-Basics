9baa87714259515e65ac2eb09d5d77b82d203c77
sudheerchoudalla <92075037+sudheerchoudalla@users.noreply.github.com>
2021-10-23 10:59:23 +0530

    Add files via upload

---
+++ adding 2 point values.c
@@ -0,0 +1,17 @@
+/*ADDING TWO POINT VALUES AND PRINT THE RESULT*/\
+#include<stdio.h>
+void main()
+{
+	//variables declaration
+	float number1,number2,sum;
+	
+	//initilization
+	number1 = 10.5;
+	number2 = 9.5;
+	
+	//caculation
+	sum = number1+number2; // 10.5+9.5 = 20
+	
+	// output
+	printf("sum of %f and %f: %f",number1,number2,sum);
+}
